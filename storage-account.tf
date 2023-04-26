8ebd8517006ffb222c962f34e00435a6bba97228
karan2077 <131740889+karan2077@users.noreply.github.com>
2023-04-26 10:51:41 +0530

    Update storage-account.tf

--- storage-account.tf
+++ storage-account.tf
@@ -1,5 +1,5 @@
-resource "azurerm_storage_account" "azurerm_storage_account_example" {
-  name                     = "karan"
+resource "azurerm_storage_account" "azurerm_storage_account_karan2077" {
+  name                     = "karan2077"
   resource_group_name      = "github"
   location                 = "West Europe"
   account_tier             = "Standard"
